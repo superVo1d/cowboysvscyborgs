66c1e1279ebee57e7d7901c721e71a9537d4a3a2
superVoid <supervoid18@gmail.com>
2020-05-01 02:37:01 +0300

    deck bug fixed

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useSprings, animated, interpolate } from 'react-spring'
 import { useGesture } from 'react-with-gesture'
 import '../deck.css'
@@ -20,19 +20,20 @@ const Deck = (props) => {
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
   const bind = useGesture(({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
     // If you flick hard enough it should trigger the card to fly out
-    const trigger = velocity > 0.05
+    const trigger = velocity > 0.01
     // Direction should either point left or right
     const dir = xDir < 0 ? -1 : 1
     // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-    if (!down && trigger) {
-      gone.add(index)
-      setCurrentCard(index)
-    }
+    if (!down && trigger) gone.add(index)
     // useSprings.set reconfigures the springs
     set(i => {
       // We're only interested in changing spring-data for the current spring
       if (index !== i) return
       const isGone = gone.has(index)
+
+  	  if (isGone) {
+	  	setCurrentCard(state => state - 1);
+  	  }
 
       // When a card is gone it flys out left or right, otherwise it's either dragged to delta, or goes back to zero
       const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0
@@ -40,14 +41,8 @@ const Deck = (props) => {
       const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0)
       // Active cards lift up a bit
       const scale = down ? 1.1 : 1
-
-      //if (isGone) {
-        //setCurrentCard(state => (state - 1) % props.cards.length)
-      //}
-
       return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
     })
-
     if (!down && gone.size === props.cards.length) {
     	setTimeout(() => gone.clear() || set(i => to(i)), 300)
     	setCurrentCard(props.cards.length)
@@ -68,7 +63,7 @@ const Deck = (props) => {
 				    </>
 				))}
 			</div>
-			    {(currentCard > 0) &&
+			    {currentCard &&
 				    <div className="card-meta px-3 px-sm-5">
 					    <h2 className="card-title text-white">{props.cards[currentCard - 1]['title']}</h2>
               <div className="card-description">
