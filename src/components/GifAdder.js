693cfbb153a75edc36d52265b1a1a6f458ee3f51
superVoid <supervoid18@gmail.com>
2020-05-05 14:55:12 +0300

    gif adder fixed

--- src/components/GifAdder.js
+++ src/components/GifAdder.js
@@ -5,47 +5,41 @@ const path = '/images/gif/'
 let arrayOfGifs = []
 
 while (arrayOfGifs.length < 15){
-    var r = Math.floor(Math.random() * 38) + 1;
-    if(arrayOfGifs.indexOf(r) === -1) arrayOfGifs.push(r);
+    var id = Math.floor(Math.random() * 38) + 1;
+    if(arrayOfGifs.indexOf(id) === -1) arrayOfGifs.push(id);
 }
 
 const GifAdder = () => {
-
-  let interval;
 
   const [gifsToShow, setGifsToShow] = useState([]);
 
   const [count, setCount] = useState(0);
 
   function tick() {
-    if (count <= 15) {
-
-      //setCount(count + 1);
-
-      setCount(prev => {
-        console.log(prev, count)
+    setCount(prev => {
+      if (prev <= 15) {
         return prev + 1
-      });
-      
-      setGifsToShow(() => {
-        console.log(gifsToShow)
-        return [...gifsToShow, arrayOfGifs[count]]
-      })
-      console.log(gifsToShow)
-    } else {
-      setCount(2);
-    }
+      }
+      else {
+        return 0
+      }
+    })
+
+    setGifsToShow(() => {
+      return count === 0 ? [arrayOfGifs[count]] : [...gifsToShow, arrayOfGifs[count]]
+    })
   }
 
   useEffect(() => {
-    interval = setInterval(() => tick(), 3000);
-  }, () => clearInterval(interval))
+    const interval = setInterval(() => tick(), 2000);
+    return () => clearInterval(interval)
+  })
 
   return gifsToShow.map((item, i) => {
          return (<img  key={i}
                        src={path + item + '.gif'} 
                  className="position-fixed" 
-                     style={{top: Math.floor(Math.random() * 80) + '%', 
+                     style={{top: Math.floor(Math.random() * 80) + '%',
                             left: Math.floor(Math.random() * 80) + '%',
                           zIndex: 10 + Math.floor(Math.random() * arrayOfGifs.length),
                        maxHeight: "300px",
