ddde8800249ffb6f5bf167f21c025f564422414a
superVo1d <supervoid18@gmail.com>
2021-03-23 13:58:40 +0300

    Updates

---
+++ precache-manifest.8c751e306fbec6af4e19eca00102585e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00488e531442c7f5e76eead6f4b85358",
+    "url": "/cowboysvscyborgs/index.html"
+  },
+  {
+    "revision": "6049366e3ed6fb9e2c76",
+    "url": "/cowboysvscyborgs/static/css/main.ed42fd5e.chunk.css"
+  },
+  {
+    "revision": "9660aa84d7457ef920e2",
+    "url": "/cowboysvscyborgs/static/js/2.671a543e.chunk.js"
+  },
+  {
+    "revision": "90fb4deb912422b947a6490378160cce",
+    "url": "/cowboysvscyborgs/static/js/2.671a543e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6049366e3ed6fb9e2c76",
+    "url": "/cowboysvscyborgs/static/js/main.6dc19824.chunk.js"
+  },
+  {
+    "revision": "e1ad9760ad2cf6276a1d",
+    "url": "/cowboysvscyborgs/static/js/runtime-main.5592e9e5.js"
+  },
+  {
+    "revision": "6f586d0063dc5635a913782659c40a0d",
+    "url": "/cowboysvscyborgs/static/media/FuturaPT-Heavy.6f586d00.eot"
+  },
+  {
+    "revision": "8b9416a7a903a8734f22e4fd043f74e9",
+    "url": "/cowboysvscyborgs/static/media/FuturaPT-Heavy.8b9416a7.ttf"
+  },
+  {
+    "revision": "ec50ee1633d1684fd0431d772c35cca8",
+    "url": "/cowboysvscyborgs/static/media/FuturaPT-Heavy.ec50ee16.woff"
+  },
+  {
+    "revision": "540c81bc4ca37ae1efe4b60de51e753f",
+    "url": "/cowboysvscyborgs/static/media/FuturaPT-Medium.540c81bc.ttf"
+  },
+  {
+    "revision": "995c64e152e8dc077222766889194951",
+    "url": "/cowboysvscyborgs/static/media/FuturaPT-Medium.995c64e1.woff"
+  },
+  {
+    "revision": "a61e39ce10e6f9114cbca70f85fbb10b",
+    "url": "/cowboysvscyborgs/static/media/FuturaPT-Medium.a61e39ce.eot"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/cowboysvscyborgs/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/cowboysvscyborgs/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/cowboysvscyborgs/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  }
+]);
